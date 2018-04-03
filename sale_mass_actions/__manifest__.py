10fdc4f1540268216db56cc1021722dff28f159f
Mahmoud Abdel Latif <mah008@me.com>
2018-04-03 20:54:07 +0200

    Update __manifest__.py

--- sale_mass_actions/__manifest__.py
+++ sale_mass_actions/__manifest__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 Mahmoud Abdel Latif (<http://www.Mah007.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
 {
     'name': 'Sale Order Mass Action',
     'summary': 'Allows confirmation or Draft of multiple sale orders in one click',
